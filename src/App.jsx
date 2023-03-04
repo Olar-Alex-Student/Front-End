226f90aee5ad36107e9279e9f7e729f954d7bec3
Arekuso-kun <alexodrino@gmail.com>
2023-03-04 23:54:52 +0200

    cleaner code

--- src/App.jsx
+++ src/App.jsx
@@ -18,51 +18,52 @@ function App() {
             </button>
           </div> : null
       }
-      <div className="container p-3">
-        <ul className="nav nav-fill rounded-5 bg-primary">
-          <li className="nav-item">
-            <a href="" className="nav-link fw-bold text-light">
-              <img src={logo} width="50" height="50"
-                className="rounded-pill d-inline-block align-top" alt="" />
-              Bizonii
-            </a>
-          </li>
-          <li className="nav-item">
-            <a href="" className="nav-link text-light active">Home</a>
-          </li>
-          <li className="nav-item">
-            <a href="" className="nav-link text-light">History</a>
-          </li>
-          <li className="nav-item">
-            <a href="" className="nav-link text-light">Forms</a>
-          </li>
-          <li className="nav-item">
-            <a href="" className="nav-link text-light">Upgrade</a>
-          </li>
-          <li className="nav-item">
-            <a href="" className="nav-link text-light">Login</a>
-          </li>
-        </ul>
+      <div className="container">
+        <Nav variant='pills' className="nav-fill rounded-pill bg-primary">
+          <Nav.Item>
+            <Nav.Link href="#/link" className='text-secondary fw-bold'>
+              <img src={logo} width="46" height="46"
+                className="rounded-pill d-inline-block align-top mx-2" alt="" />
+              Bizonii</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href="#/link" className='text-secondary rounded-pill active'>Home</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href="#/link" className='text-secondary'>History</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href="#/link" className='text-secondary'>Forms</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href="#/link" className='text-secondary'>Upgrade</Nav.Link>
+          </Nav.Item>
+
+          <Nav.Item>
+            <Nav.Link href="#/link" className='text-secondary'>Login</Nav.Link>
+          </Nav.Item>
+        </Nav>
       </div>
       {
         !toggle ?
-      <div className="container">
-        <div className="d-flex justify-content-center align-content-center p-4">
-          <div className="box bg-primary p-5 d-flex justify-content-center align-items-center flex-column gap-5 hide">
-            <div className="title">
-
-              <span className="toggle fw-bold hide">Forms</span>
+          <div className="container d-flex justify-content-center p-4">
+            <div className="box bg-primary p-5 d-flex justify-content-center align-items-center flex-column gap-5">
+              <div className="title text-dark fw-bold">
+                <span>Forms</span>
+              </div>
+              <p className="text-center text-secondary fs-5">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
+                similique nihil, soluta nulla ratione minus modi nostrum dicta illo obcaecati, necessitatibus rem,
+                odit voluptate. Quod, consequatur repellat! Eligendi, et numquam!</p>
+              <div className="buttons d-flex gap-5">
+                <Button className="rounded-pill fw-bold">Login</Button>
+                <Button className="rounded-pill fw-bold">Sign Up</Button>
+              </div>
             </div>
-            <h4 className="toggle text-center text-secondary hide">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia
-              similique nihil, soluta nulla ratione minus modi nostrum dicta illo obcaecati, necessitatibus rem,
-              odit voluptate. Quod, consequatur repellat! Eligendi, et numquam!</h4>
-            <div className="toggle buttons d-flex gap-5 hide">
-              <button type="button" className="btn rounded-pill btn-secondary fw-bold">Login</button>
-              <button type="button" className="btn rounded-pill btn-secondary fw-bold">Sign Up</button>
-            </div>
-          </div>
-        </div>
-      </div> : null
+          </div> : null
       }
     </div>
   )
