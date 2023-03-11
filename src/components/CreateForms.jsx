6ee98c9fa939c7c1c49c33bcfadd2b8173483a93
Arekuso-kun <alexodrino@gmail.com>
2023-03-11 20:29:31 +0200

    create form

--- src/components/CreateForms.jsx
+++ src/components/CreateForms.jsx
@@ -1,8 +1,59 @@
 import React, { useState, useEffect } from 'react'
-import logo from "../images/bison_logo.png"
-import { Navbar, Nav, Button, Table, Container, Modal, InputGroup, Form } from 'react-bootstrap';
+import { Navbar, Nav, Button, Table, Container, Modal, InputGroup, Form, ListGroup, Tab } from 'react-bootstrap';
 
 export const CreateForms = () => {
+
+  const [dynamic_fields, setDynamicFields] = useState({
+    'Nume': {
+      'placeholder': 'nume',
+      'mandatory': true,
+      'documentKeywords': 'Nume, Last name'
+    },
+    'Prenume': {
+      'placeholder': 'prenume',
+      'mandatory': true,
+      'documentKeywords': 'Prenume, First name'
+    },
+    'CNP': {
+      'placeholder': 'cnp',
+      'mandatory': true,
+      'documentKeywords': 'CNP, Social Number'
+    }
+  });
+
+  const [oldLabel, setOldLabel] = useState('')
+  const [label, setLabel] = useState('')
+  const [placeholder, setPlaceholder] = useState('')
+  const [mandatory, setMandatory] = useState(false)
+  const [documentKeywords, setDocumentKeywords] = useState('')
+
+  useEffect(() => {
+    const updatedDynamicFields = { ...dynamic_fields };
+    if(oldLabel != label && oldLabel in updatedDynamicFields) 
+    {
+      updatedDynamicFields[label] = updatedDynamicFields[oldLabel];
+      delete updatedDynamicFields[oldLabel];
+    }
+    if (label in updatedDynamicFields) {
+      updatedDynamicFields[label].placeholder = placeholder;
+      updatedDynamicFields[label].mandatory = mandatory;
+      updatedDynamicFields[label].documentKeywords = documentKeywords;
+      setDynamicFields(updatedDynamicFields);
+    }
+  }, [label, placeholder, mandatory, documentKeywords]);
+
+  function loadDynamicField(event, key) {
+    event.preventDefault();
+    setOldLabel(key);
+    setLabel(key);
+    setPlaceholder(dynamic_fields[key].placeholder);
+    setMandatory(dynamic_fields[key].mandatory); 
+    setDocumentKeywords(dynamic_fields[key].documentKeywords); 
+  }
+
+  function addNewLabel() {
+    setDynamicFields({...dynamic_fields, 'NEW': {'placeholder': '', 'mandatory': false, 'documentKeywords': ''}});
+  }
 
   return (
     <>
@@ -13,15 +64,36 @@ export const CreateForms = () => {
           </div>
           <Container>
             <form className="row g-3 p-3 rounded-5 bg-secondary">
-              <div class="col-6 mb-3">
-                <label for="InputTitle" class="form-label">Title</label>
-                <input type="email" class="form-control" id="InputTitle" placeholder="Title" />
+              <div className="col-lg-12 mb-3">
+                <label htmlFor="input-title" className="form-label">Title</label>
+                <input type="text" className="form-control" id="input-title" placeholder="Title" />
               </div>
-              <div class="col-6 mb-3">
-                <label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-                <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+              <div className="col-lg-6 mb-3">
+                <Form.Label>Dynamic Fields</Form.Label>
+                <ListGroup>
+                  {Object.keys(dynamic_fields).map((key, i) => (
+                    <ListGroup.Item action onClick={(event) => { loadDynamicField(event, key) }}>{key}</ListGroup.Item>
+                  ))}
+                </ListGroup>
+                <div className='d-flex align-items-center justify-content-around'>
+                <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={addNewLabel}>Add new</Button>
+                <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={addNewLabel}>Delete</Button>
+                </div>
               </div>
-              <div className="col-12">
+              <div className="col-lg-6 mb-3">
+                <Form.Label>Label</Form.Label>
+                <Form.Control className="mb-3" type="text" placeholder="Label" value={label} onChange={(e) => {setOldLabel(label); setLabel(e.target.value)}} />
+                <Form.Label>Placeholder keyword</Form.Label>
+                <Form.Control className="mb-3" type="text" placeholder="Placeholder keyword" value={placeholder} onChange={(e) => setPlaceholder(e.target.value)} />
+                <Form.Check className="mb-3" label="Mandatory" id="checkbox-id" checked={mandatory}  onChange={(e) => setMandatory(e.target.checked)} />
+                <Form.Label>Document keywords</Form.Label>
+                <Form.Control as="textarea" placeholder="example, example, example ..." rows={3} value={documentKeywords} onChange={(e) => setDocumentKeywords(e.target.value)} />
+              </div>
+              <div className="col-lg-6 mb-3">
+                <label htmlFor="input-content" className="form-label">Content</label>
+                <textarea className="form-control" id="input-content" rows="3"></textarea>
+              </div>
+              <div className="col-lg-12">
                 <div className='d-flex align-items-center justify-content-center'>
                   <Button type="submit" className='custom-button custom-button-inverted medium-button-size rounded-pill fw-bold'>Create!</Button>
                 </div>
