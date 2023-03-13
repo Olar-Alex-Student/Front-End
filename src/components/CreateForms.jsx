239a0b996d9889914ac12310b4b245e090341a72
Arekuso-kun <alexodrino@gmail.com>
2023-03-13 16:51:21 +0200

    create form, token integration left to be done

--- src/components/CreateForms.jsx
+++ src/components/CreateForms.jsx
@@ -1,16 +1,30 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect, useRef, useMemo } from 'react'
 import { Navbar, Nav, Button, Table, Container, Modal, InputGroup, Form, ListGroup, Tab } from 'react-bootstrap';
 import axios from "axios";
+import JoditEditor from 'jodit-react';
 
 export const CreateForms = () => {
 
-  const url = "https://bizoni-backend-apis.azurewebsites.net/api/v1/users/1d3294ee-324c-4458-bd53-24d35d2a340f/forms/";
-  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhcmVrdXNvNTAwMEBnbWFpbC5jb20iLCJleHAiOjE2Nzg2MzgxMzd9.DExXR47VwGcSx353cd0ihXqHtBjUr63ASfodV9KK44I';
+  const url = "https://bizoni-backend-apis.azurewebsites.net/api/v1/users/66cbb2dc-2202-411c-8395-72e996bd6ccc/forms/";
+  const token = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiJhc2QzMzNAZ21haWwuY29tIiwiZXhwIjoxNjc4NzIwMzc5fQ.alabAy_GUep4H0YhML6_W3mzEMb5ppJYfRp8MVX5SwY';
   const headers = {
     Authorization: `Bearer ${token}`
   };
 
   const [error, setError] = useState("");
+
+  const editor = useRef(null);
+
+  const [data_retension_period, setData_retension_period] = useState(30)
+
+  const [selectedSection, setSelectedSection] = useState(null);
+
+  const [scan_document_type, setScan_document_type] = useState('')
+  const [content, setContent] = useState('');
+
+  useEffect(() => {
+    console.log(content)
+  }, [content])
 
   const [dynamic_fields, setDynamicFields] = useState({
     'Nume': {
@@ -43,7 +57,22 @@ export const CreateForms = () => {
     }
   });
 
+  const [sections, setSections] = useState([
+    {
+      "scan_document_type": "student_card",
+      "text": "Studentul <nume> , din grupa , anul <an>."
+    },
+    {
+      "scan_document_type": "identity_card",
+      "text": "Cu CNP <cnp>, seria , nr."
+    }
+  ]);
+
+  const [title, setTitle] = useState('')
+
+  const [selectedLabel, setSelectedLabel] = useState('')
   const [oldLabel, setOldLabel] = useState('')
+
   const [label, setLabel] = useState('')
   const [placeholder, setPlaceholder] = useState('')
   const [type, setType] = useState('')
@@ -67,6 +96,17 @@ export const CreateForms = () => {
     }
   }, [label, placeholder, type, mandatory, keywords, options]);
 
+  useEffect(() => {
+    const updatedSections = sections.map((section, i) => {
+      if (i === selectedSection) {
+        return { ...section, text: content, scan_document_type: scan_document_type };
+      }
+      return section;
+    });
+    setSections(updatedSections);
+
+  }, [content, scan_document_type]);
+
   function loadDynamicField(event, key) {
     event.preventDefault();
     setOldLabel(key);
@@ -78,18 +118,40 @@ export const CreateForms = () => {
     setOptions(dynamic_fields[key].options);
   }
 
-  function addNewLabel() {
-    setDynamicFields({ ...dynamic_fields, 'NEW': { 'placeholder': '', 'type': '', 'mandatory': false, 'keywords': '', 'options': '' } });
+  function loadSections(event, index) {
+    event.preventDefault();
+    setContent(sections[index].text)
+    setScan_document_type(sections[index].scan_document_type)
   }
 
-  function json2array(json){
+  function addNewLabel() {
+    setDynamicFields({ ...dynamic_fields, [`NEW ${Object.keys(dynamic_fields).length + 1}`]: { 'placeholder': '', 'type': '', 'mandatory': false, 'keywords': '', 'options': '' } });
+  }
+
+  function addNewSection() {
+    setSections([...sections, { 'scan_document_type': '', 'text': '' }]);
+  }
+
+  function deleteLabel() {
+    const updatedDynamicFields = { ...dynamic_fields };
+    delete updatedDynamicFields[selectedLabel];
+    setDynamicFields(updatedDynamicFields);
+  }
+
+  function deleteSection() {
+    const updatedSections = [...sections];
+    delete updatedSections[selectedSection];
+    setSections(updatedSections);
+  }
+
+  function json2array(json) {
     var result = [];
     var keys = Object.keys(json);
-    keys.forEach(function(key){
-        result.push(json[key]);
+    keys.forEach(function (key) {
+      result.push(json[key]);
     });
     return result;
-}
+  }
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -97,34 +159,29 @@ export const CreateForms = () => {
     Object.entries(dynamic_fields).forEach(([key, value]) => {
       const options_array = dynamic_fields[key].options.toString().split(',')
       const options_trimmedArray = [];
+
       const keywords_array = dynamic_fields[key].keywords.toString().split(',')
       const keywords_trimmedArray = [];
+
       for (let i = 0; i < options_array.length; i++) {
         const options_trimmedElement = options_array[i].trim();
         options_trimmedArray.push(options_trimmedElement);
       }
+
       for (let i = 0; i < keywords_array.length; i++) {
         const keywords_trimmedElement = keywords_array[i].trim();
         keywords_trimmedArray.push(keywords_trimmedElement);
       }
+
       dynamic_fields[key].options = options_trimmedArray
       dynamic_fields[key].keywords = keywords_trimmedArray
     })
     console.log(dynamic_fields)
     const dynamic_fields_array = json2array(dynamic_fields)
     const data = {
-      "title": "Fertre fdsf",
-      "delete_form_date": "1680619408",
-      "sections": [
-        {
-          "scan_document_type": "student_card",
-          "text": "Studentul."
-        },
-        {
-          "scan_document_type": "identity_card",
-          "text": "Cu CNP, seria , nr."
-        }
-      ],
+      "title": title,
+      "delete_form_date": Math.floor(Date.now() / 1000 + 86400 * data_retension_period), // 2592000 = 30 days Unix epoch time
+      "sections": sections,
       "dynamic_fields": dynamic_fields_array
     }
     console.log(data)
@@ -149,19 +206,21 @@ export const CreateForms = () => {
           <Container>
             <form className="row g-3 p-3 rounded-5 bg-secondary">
               <div className="col-lg-12 mb-3">
-                <label htmlFor="input-title" className="form-label">Title</label>
-                <input type="text" className="form-control" id="input-title" placeholder="Title" />
+                <Form.Label>Title</Form.Label>
+                <Form.Control className="mb-3" type="text" placeholder="Title" onChange={(e) => { setTitle(e.target.value) }} />
+                {/* <label htmlFor="input-title" className="form-label">Title</label>
+                <input type="text" className="form-control" id="input-title" placeholder="Title" /> */}
               </div>
               <div className="col-lg-6 mb-3">
                 <Form.Label>Dynamic Fields</Form.Label>
                 <ListGroup>
                   {Object.keys(dynamic_fields).map((key, i) => (
-                    <ListGroup.Item key={key} action onClick={(event) => { loadDynamicField(event, key) }}>{key}</ListGroup.Item>
+                    <ListGroup.Item key={key} action onClick={(event) => { loadDynamicField(event, key); setSelectedLabel(key) }}>{key}</ListGroup.Item>
                   ))}
                 </ListGroup>
                 <div className='d-flex align-items-center justify-content-around'>
                   <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={addNewLabel}>Add new</Button>
-                  <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={addNewLabel}>Delete</Button>
+                  <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={deleteLabel}>Delete</Button>
                 </div>
               </div>
               <div className="col-lg-6 mb-3">
@@ -190,8 +249,38 @@ export const CreateForms = () => {
                 <Form.Control as="textarea" placeholder="keyword1, keyword2, keyword3 ..." rows={2} value={keywords} onChange={(e) => setKeywords(e.target.value)} />
               </div>
               <div className="col-lg-6 mb-3">
-                <label htmlFor="input-content" className="form-label">Content</label>
-                <textarea className="form-control" id="input-content" rows="3"></textarea>
+                <Form.Label>Sections</Form.Label>
+                <ListGroup>
+                  {sections.map((section, index) => (
+                    <ListGroup.Item key={index} action onClick={(event) => { loadSections(event, index); setSelectedSection(index) }}>{`Section ${index + 1}`}</ListGroup.Item>
+                  ))}
+                </ListGroup>
+                <div className='d-flex align-items-center justify-content-around'>
+                  <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={addNewSection}>Add new</Button>
+                  <Button className='m-3 custom-button custom-button-inverted medium-button-size rounded-pill fw-bold' variant="primary" onClick={deleteSection}>Delete</Button>
+                </div>
+                <Form.Label>Data Retension Period</Form.Label>
+                <Form.Control className="mb-3" type="number" placeholder="Data Retension Period" value={data_retension_period} onChange={(e) => setData_retension_period(Math.max(1, Math.min(60, Number(e.target.value))))} />
+              </div>
+              <div className="col-lg-6 mb-3">
+                <Form.Label>Content</Form.Label>
+                <JoditEditor
+                  ref={editor}
+                  value={content}
+                  onBlur={newContent => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+                  onChange={newContent => { }}
+                />
+                <Form.Label>Scan document type</Form.Label>
+                <Form.Select className="mb-3" value={scan_document_type} onChange={(e) => setScan_document_type(e.target.value)} defaultValue="none">
+                  {/* <option>Scan document type</option> */}
+                  <option value="none">None</option>
+                  <option value="identity_card">Identity Card</option>
+                  <option value="passport">Passport</option>
+                  <option value="birth_certificate">Birth Certificate</option>
+                  <option value="vehicle_identity_card">Vehicle Identity Card</option>
+                  <option value="student_card">Student Card</option>
+                  <option value="any_document"> Any Document</option>
+                </Form.Select>
               </div>
               <div className="col-lg-12">
                 <div className='d-flex align-items-center justify-content-center'>
