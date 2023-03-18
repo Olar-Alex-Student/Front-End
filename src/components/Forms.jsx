c6df3d46dbf9aa9056889a7a41ac8dd1f0ddad78
Arekuso-kun <alexodrino@gmail.com>
2023-03-18 19:38:28 +0200

    qr code

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,17 +1,23 @@
 import React, { useState, useEffect } from 'react'
 import { Navbar, Nav, Button, Table, Container, Modal } from 'react-bootstrap';
 import axios from "axios";
+import QRCode from 'react-qr-code';
 
 export const Forms = () => {
   const [show, setShow] = useState(false);
-
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [show_qrcode, setShow_qrcode] = useState(false);
+  const handleClose_qrcode = () => setShow_qrcode(false);
+  const handleShow_qrcode = () => setShow_qrcode(true);
+
+  const [qrcode, setQrcode] = useState('');
 
   const [error, setError] = useState("");
 
   const [titles, setTitles] = useState([]);
-  const [delete_id, setDelete_id] = useState('');  
+  const [currentID, setCurrentID] = useState('');
 
   const token = sessionStorage.getItem('token');
   const id = sessionStorage.getItem('id');
@@ -36,10 +42,11 @@ export const Forms = () => {
   };
 
   async function handleDelete() {
-    const delete_url = `${url}${delete_id}`;
+    const delete_url = `${url}${currentID}`;
     try {
       const response = await axios.delete(delete_url, { headers: headers });
       console.log(response.data); // Handle successful login
+      setQrcode(response.data);
       console.log('deleted')
       window.location.reload(false);
     } catch (err) {
@@ -47,6 +54,27 @@ export const Forms = () => {
       console.log(err)
     }
   };
+
+  // async function handleQrCode() {
+  //   const qrcode_url = `https://bizoni-backend-apis.azurewebsites.net/api/v1/users/${id}/utilitygetQrCode/${window.location.href}/forms/fill/${currentID}`;
+  //   console.log(qrcode_url)
+  //   try {
+  //     const response = await axios.get(qrcode_url, { headers: headers });
+  //     console.log(response.data); // Handle successful login
+  //     console.log('qrcode')
+
+  //     console.log(atob(response.data))
+
+  //     image.onload = function(){
+  //         URL.revokeObjectURL(this.src); // release the blob URL once the image is loaded
+  //     }
+  //     image.src = blobURL;
+
+  //   } catch (err) {
+  //     console.log('error'); // error.response.data.message
+  //     console.log(err)
+  //   }
+  // };
 
   useEffect(() => {
     let ignore = false;
@@ -64,37 +92,38 @@ export const Forms = () => {
           </div>
           <Button href="/forms/create" className="custom-button medium-button-size rounded-pill fw-bold align-self-start">Create New Form</Button>
           <Container className='bg-secondary rounded-5 p-3'>
-          {titles.length ? 
-            <Table hover className='bg-secondary'>
-              <thead>
-                <tr>
-                  <th>#</th>
-                  <th>Form Title</th>
-                  {/* <th>Creation date</th>
-                  <th>Description</th> */}
-                  <th></th>
-                  <th></th>
-                </tr>
-              </thead>
-              <tbody>
-                {titles.map((title, index) => (
+            {titles.length ?
+              <Table hover className='bg-secondary'>
+                <thead>
                   <tr>
-                    <td>{index + 1}</td>
-                    <td>{title['title']}</td>
-                    {/* <td>2023-02-04</td>
-                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, similique.</td> */}
-                    <td></td>
-                    <td>
-                      <div className='d-flex gap-3 justify-content-end'>
-                        <Button href={`/forms/fill/${title['id']}`} className="rounded-pill fw-bold btn-info btn-sm">Fill </Button>
-                        <Button href={`/forms/create`} className="rounded-pill fw-bold btn-warning btn-sm">Edit</Button>
-                        <Button onClick={() => {handleShow(); setDelete_id(title['id'])}} className="rounded-pill fw-bold btn-danger btn-sm">Delete</Button>
-                      </div>
-                    </td>
+                    <th>#</th>
+                    <th>Form Title</th>
+                    {/* <th>Creation date</th>
+                  <th>Description</th> */}
+                    <th></th>
+                    <th></th>
                   </tr>
-                ))} 
-              </tbody>
-            </Table>
+                </thead>
+                <tbody>
+                  {titles.map((title, index) => (
+                    <tr>
+                      <td>{index + 1}</td>
+                      <td>{title['title']}</td>
+                      {/* <td>2023-02-04</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, similique.</td> */}
+                      <td></td>
+                      <td>
+                        <div className='d-flex gap-3 justify-content-end'>
+                          <Button onClick={() => { handleShow_qrcode(); setCurrentID(title['id']); setQrcode(`${window.location.href}/fill/${currentID}`) }} className="rounded-pill fw-bold btn-info btn-sm px-3">QR Code</Button>
+                          <Button href={`/forms/fill/${title['id']}`} className="rounded-pill fw-bold btn-success btn-sm px-3">Fill</Button>
+                          <Button href={`/forms/create/${title['id']}`} className="rounded-pill fw-bold btn-warning btn-sm px-3">Edit</Button>
+                          <Button onClick={() => { handleShow(); setCurrentID(title['id']) }} className="rounded-pill fw-bold btn-danger btn-sm px-3">Delete</Button>
+                        </div>
+                      </td>
+                    </tr>
+                  ))}
+                </tbody>
+              </Table>
               : <h1 className='d-flex text-primary gap-3 justify-content-center py-5'>You do not have any forms yet. :(</h1>}
           </Container>
           <Modal show={show} onHide={handleClose}>
@@ -106,8 +135,28 @@ export const Forms = () => {
               <Button variant="secondary" onClick={handleClose}>
                 Changed my mind
               </Button>
-              <Button variant="primary" onClick={(e) => {handleClose(e); handleDelete()}}>
+              <Button variant="primary" onClick={(e) => { handleClose(e); handleDelete() }}>
                 Delete it!
+              </Button>
+            </Modal.Footer>
+          </Modal>
+
+          <Modal show={show_qrcode} onHide={handleClose_qrcode}>
+            <Modal.Header closeButton>
+              <Modal.Title>Enjoy your QR Code</Modal.Title>
+            </Modal.Header>
+            <Modal.Body>
+              <div style={{ height: "auto", margin: "0 auto", maxWidth: 256, width: "100%" }}>
+                <QRCode
+                  size={256}
+                  value={qrcode}
+                  viewBox={`0 0 256 256`}
+                />
+              </div>
+            </Modal.Body>
+            <Modal.Footer>
+              <Button variant="primary" onClick={handleClose_qrcode}>
+                Close
               </Button>
             </Modal.Footer>
           </Modal>
