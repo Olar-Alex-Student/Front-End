45d7557f35a921f131cb004518b93e62f7350375
Arekuso-kun <alexodrino@gmail.com>
2023-03-18 18:21:17 +0200

    form list & delete button

--- src/components/Forms.jsx
+++ src/components/Forms.jsx
@@ -1,11 +1,59 @@
 import React, { useState, useEffect } from 'react'
 import { Navbar, Nav, Button, Table, Container, Modal } from 'react-bootstrap';
+import axios from "axios";
 
 export const Forms = () => {
   const [show, setShow] = useState(false);
 
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
+
+  const [error, setError] = useState("");
+
+  const [titles, setTitles] = useState([]);
+  const [delete_id, setDelete_id] = useState('');  
+
+  const token = sessionStorage.getItem('token');
+  const id = sessionStorage.getItem('id');
+  const url = `https://bizoni-backend-apis.azurewebsites.net/api/v1/users/${id}/forms/`;
+  const headers = {
+    // 'accept': 'application/json',
+    Authorization: `Bearer ${token}`
+  };
+
+  async function handleGet() {
+    console.log(token)
+    try {
+      const response = await axios.get(url, { headers: headers });
+      console.log(response.data); // Handle successful login
+      setTitles(response.data['form_list'])
+      console.log(titles)
+      console.log('success')
+    } catch (err) {
+      console.log('error'); // error.response.data.message
+      console.log(err)
+    }
+  };
+
+  async function handleDelete() {
+    const delete_url = `${url}${delete_id}`;
+    try {
+      const response = await axios.delete(delete_url, { headers: headers });
+      console.log(response.data); // Handle successful login
+      console.log('deleted')
+      window.location.reload(false);
+    } catch (err) {
+      console.log('error'); // error.response.data.message
+      console.log(err)
+    }
+  };
+
+  useEffect(() => {
+    let ignore = false;
+
+    if (!ignore) handleGet()
+    return () => { ignore = true; }
+  }, []);
 
   return (
     <>
@@ -16,58 +64,38 @@ export const Forms = () => {
           </div>
           <Button href="/forms/create" className="custom-button medium-button-size rounded-pill fw-bold align-self-start">Create New Form</Button>
           <Container className='bg-secondary rounded-5 p-3'>
+          {titles.length ? 
             <Table hover className='bg-secondary'>
               <thead>
                 <tr>
                   <th>#</th>
                   <th>Form Title</th>
-                  <th>Creation date</th>
-                  <th>Description</th>
-                  <th>Actions</th>
+                  {/* <th>Creation date</th>
+                  <th>Description</th> */}
+                  <th></th>
+                  <th></th>
                 </tr>
               </thead>
               <tbody>
-                <tr>
-                  <td>1</td>
-                  <td>Mark</td>
-                  <td>2023-07-03</td>
-                  <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, similique.</td>
-                  <td>
-                    <div className='d-flex gap-3'>
-                      <Button href="/forms/1/view" className="rounded-pill fw-bold btn-info btn-sm">View</Button>
-                      <Button href="/forms/1/edit" className="rounded-pill fw-bold btn-warning btn-sm">Edit</Button>
-                      <Button onClick={handleShow} className="rounded-pill fw-bold btn-danger btn-sm">Delete</Button>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td>2</td>
-                  <td>Jacob</td>
-                  <td>2023-02-04</td>
-                  <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, similique.</td>
-                  <td>
-                    <div className='d-flex gap-3'>
-                      <Button href="/forms/2/view" className="rounded-pill fw-bold btn-info btn-sm">View</Button>
-                      <Button href="/forms/2/edit" className="rounded-pill fw-bold btn-warning btn-sm">Edit</Button>
-                      <Button onClick={handleShow} className="rounded-pill fw-bold btn-danger btn-sm">Delete</Button>
-                    </div>
-                  </td>
-                </tr>
-                <tr>
-                  <td>3</td>
-                  <td>Larry the Bird</td>
-                  <td>2023-10-02</td>
-                  <td>Lorem ipsum dolor sit amet.</td>
-                  <td>
-                    <div className='d-flex gap-3'>
-                      <Button href="/forms/3/view" className="rounded-pill fw-bold btn-info btn-sm">View</Button>
-                      <Button href="/forms/2/edit" className="rounded-pill fw-bold btn-warning btn-sm">Edit</Button>
-                      <Button onClick={handleShow} className="rounded-pill fw-bold btn-danger btn-sm">Delete</Button>
-                    </div>
-                  </td>
-                </tr>
+                {titles.map((title, index) => (
+                  <tr>
+                    <td>{index + 1}</td>
+                    <td>{title['title']}</td>
+                    {/* <td>2023-02-04</td>
+                    <td>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, similique.</td> */}
+                    <td></td>
+                    <td>
+                      <div className='d-flex gap-3 justify-content-end'>
+                        <Button href={`/forms/fill/${title['id']}`} className="rounded-pill fw-bold btn-info btn-sm">Fill </Button>
+                        <Button href={`/forms/create`} className="rounded-pill fw-bold btn-warning btn-sm">Edit</Button>
+                        <Button onClick={() => {handleShow(); setDelete_id(title['id'])}} className="rounded-pill fw-bold btn-danger btn-sm">Delete</Button>
+                      </div>
+                    </td>
+                  </tr>
+                ))} 
               </tbody>
             </Table>
+              : <h1 className='d-flex text-primary gap-3 justify-content-center py-5'>You do not have any forms yet. :(</h1>}
           </Container>
           <Modal show={show} onHide={handleClose}>
             <Modal.Header closeButton>
@@ -78,7 +106,7 @@ export const Forms = () => {
               <Button variant="secondary" onClick={handleClose}>
                 Changed my mind
               </Button>
-              <Button variant="primary" onClick={handleClose}>
+              <Button variant="primary" onClick={(e) => {handleClose(e); handleDelete()}}>
                 Delete it!
               </Button>
             </Modal.Footer>
