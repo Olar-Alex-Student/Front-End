eb4b646c93f550422f628ef26e9b5f44b80988fb
Arekuso-kun <alexodrino@gmail.com>
2023-03-22 20:59:02 +0200

    create form ex

--- src/components/CreateForms.jsx
+++ src/components/CreateForms.jsx
@@ -54,7 +54,23 @@ export const CreateForms = () => {
       'type': 'single-choice',
       'mandatory': true,
       'keywords': 'An, Year',
-      'options': '2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024'
+      'options': '1, 2, 3, 4'
+    },
+    {
+      'label': 'Seria',
+      'placeholder': 'seria',
+      'type': 'text',
+      'mandatory': true,
+      'keywords': 'Seria',
+      'options': ''
+    },
+    {
+      'label': 'Nr',
+      'placeholder': 'nr',
+      'type': 'number',
+      'mandatory': true,
+      'keywords': 'numar, nr',
+      'options': ''
     }
   ]);
 
@@ -65,7 +81,7 @@ export const CreateForms = () => {
     },
     {
       "scan_document_type": "identity_card",
-      "text": "Cu CNP {cnp}, seria , nr."
+      "text": "Cu CNP {cnp}, seria {seria}, nr. {nr}"
     }
   ]);
 
